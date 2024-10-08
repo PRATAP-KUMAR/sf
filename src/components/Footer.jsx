5e592b3a2f4faa1704694ead3864e58533e8e243
PRATAP-KUMAR <pratap@fastmail.fm>
2024-10-08 21:21:17 +0530

    on footer use links for home and discover pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,27 +2,24 @@ import { LiaSearchPlusSolid } from "react-icons/lia";
 import { VscGlobe } from "react-icons/vsc";
 import { FaSignInAlt } from "react-icons/fa";
 import { MdOutlineLocalLibrary } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <footer className="desktop:hidden">
             <div className="bg-[#d7d7d7] fixed bottom-0 left-0 z-50 h-16 items-center flex justify-around border w-full">
-                <a className="items-center justify-center flex flex-col"
-                    href="https://pratap.live"
-                    target="_blank"
-                    rel="noreferrer"
+                <Link className="items-center justify-center flex flex-col"
+                    to={"/"}
                 >
                     <LiaSearchPlusSolid size={24} />
                     <p>Home</p>
-                </a>
-                <a className="items-center justify-center flex flex-col"
-                    href="https://pratap.live"
-                    target="_blank"
-                    rel="noreferrer"
+                </Link>
+                <Link className="items-center justify-center flex flex-col"
+                    to={"/discover"}
                 >
                     <VscGlobe size={24} />
                     <p>Discover</p>
-                </a>
+                </Link>
                 <a className="items-center justify-center flex flex-col"
                     href="https://pratap.live"
                     target="_blank"
